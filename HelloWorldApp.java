a61e4fcfa8265e4b6bd5f5f771f93aed4780464b
lorenaleao <42307217+lorenaleao@users.noreply.github.com>
2018-08-12 20:51:40 -0300

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -7,9 +7,9 @@
 
 class HelloWorldApp { //every application begins with a class definition
 	public static void main(String[] args){ //every application must contain a main method, 
-		                                    //it's similar to the main function in C and C++
-											//The main method accepts a single argument: an array of elements of type String.
-											//that way i can provide arguments through the command line to the application without having to recompile it 
+		                                //it's similar to the main function in C and C++
+						//The main method accepts a single argument: an array of elements of type String.
+						//that way i can provide arguments through the command line to the application without having to recompile it 
 		System.out.println("Hello World!"); //Display the string using the "System" class from the core library to print the "Hello World!" message to standard output.
 	}
 }
