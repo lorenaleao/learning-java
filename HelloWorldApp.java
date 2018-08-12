c8c2b82daa05d34cf42321ea91b544f33dfba0e6
lorenaleao <42307217+lorenaleao@users.noreply.github.com>
2018-08-12 20:50:38 -0300

    Add files via upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,15 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+//above we have a doc comment: /** documentation */
+
+class HelloWorldApp { //every application begins with a class definition
+	public static void main(String[] args){ //every application must contain a main method, 
+		                                    //it's similar to the main function in C and C++
+											//The main method accepts a single argument: an array of elements of type String.
+											//that way i can provide arguments through the command line to the application without having to recompile it 
+		System.out.println("Hello World!"); //Display the string using the "System" class from the core library to print the "Hello World!" message to standard output.
+	}
+}
